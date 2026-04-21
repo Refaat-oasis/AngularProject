46dfda41201438d81afa0f08e437fc74f1598801
Refaat_Oasis <ahta601@gmail.com>
2026-04-21 14:01:30 +0200

    changing environement urls

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:5118/api',
-  baseUrl: 'http://localhost:5118',
+  apiUrl: 'http://architecture.runasp.net/api',
+  baseUrl: 'http://architecture.runasp.net',
   stripePublishableKey: 'pk_test_51TMrasCe2J7orNbpe6W7cRWJTACgtclgKIW1id8aAUowvX8XGCl8fLGNms5UUIhOOLbqycdZkEZEXp06eYjX7hU900HyEcV9zH'
 };
